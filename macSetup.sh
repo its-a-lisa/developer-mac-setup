e8451446679597fa8a14d6b6d3d553dbb36e95dc
Alyssa Feola <65581330+its-a-lisa@users.noreply.github.com>
2021-02-15 22:22:15 -0500

    updates based on original setup

--- macSetup.sh
+++ macSetup.sh
@@ -46,44 +46,12 @@ read General
 
 CaskGeneralToolList=(
     google-chrome
-    firefox
-    spotify
-    github
+    lastpass
+   
 )
 if [ "$General" != "${General#[Yy]}" ] ;then
     echo Yes
     brew install --cask --appdir="/Applications" ${CaskGeneralToolList[@]}
-else
-    echo No
-fi
-
-############# Designer #############
-beginDeploy "############# Designer #############"
-echo -n "Do you wish to install Designer Tools (${bold}${green}y${reset}/${bold}${red}n${reset})? "
-read Designer
-
-CaskDesignerToolList=(
-    adobe-creative-cloud
-)
-if [ "$Designer" != "${Designer#[Yy]}" ] ;then
-    echo Yes
-    brew install --cask --appdir="/Applications" ${CaskDesignerToolList[@]}
-else
-    echo No
-fi
-
-
-############# Mobile Developer #############
-beginDeploy "############# Mobile Developer #############"
-echo -n "Do you wish to install Mobile Developer Tools (${bold}${green}y${reset}/${bold}${red}n${reset})? "
-read MobileDeveloper
-
-CaskMobileDeveloperToolList=(
-    fastlane
-)
-if [ "$MobileDeveloper" != "${MobileDeveloper#[Yy]}" ] ;then
-    echo Yes
-    brew install --cask ${CaskMobileDeveloperToolList[@]}
 else
     echo No
 fi
@@ -97,19 +65,15 @@ read DeveloperUtilities
 DeveloperUtilitiesList=(
     jq
     yarn
-    yarn-completion
-    nmap
     wget
     nvm
     bash-completion
     curl
-    openvpn
 )
 CaskDeveloperUtilitiesList=(
     postman
-    wireshark
-    # google-chrome-canary
-    # firefox-developer-edition
+    github
+    docker
 )
 if [ "$DeveloperUtilities" != "${DeveloperUtilities#[Yy]}" ] ;then
     
@@ -131,38 +95,6 @@ if [ "$DeveloperUtilities" != "${DeveloperUtilities#[Yy]}" ] ;then
     [[ -r "$(brew --prefix)/etc/profile.d/bash_completion.sh" ]] && . "$(brew --prefix)/etc/profile.d/bash_completion.sh"' >> ~/.bash_profile
 
 
-    curl -s "https://get.sdkman.io" | bash
-    echo '
-    # THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
-    export SDKMAN_DIR="/Users/mpatel/.sdkman"
-    [[ -s "/Users/mpatel/.sdkman/bin/sdkman-init.sh" ]] && source "${HOME}/.sdkman/bin/sdkman-init.sh"' >> ~/.bash_profile
-else
-    echo No
-fi
-
-
-############# Database Tools #############
-beginDeploy "############# Database Tools #############"
-echo -n "Do you wish to install Database Tools (${bold}${green}y${reset}/${bold}${red}n${reset})? "
-read Database
-
-
-DatabaseToolList=(
-    kafkacat
-)
-CaskDatabaseToolList=(
-    pgadmin4
-    studio-3t
-    graphiql
-    azure-data-studio
-)
-if [ "$Database" != "${Database#[Yy]}" ] ;then
-    echo Yes
-    brew install ${DatabaseToolList[@]}
-    brew install --cask ${CaskDatabaseToolList[@]}
-
-else
-    echo No
 fi
 
 
@@ -183,55 +115,6 @@ else
 fi
 
 
-############# DevOps #############
-beginDeploy "############# DevOps #############"
-echo -n "Do you wish to install DevOps Tools (${bold}${green}y${reset}/${bold}${red}n${reset})? "
-read DevOps
-
-
-DevOpsToolList=(
-    terraform
-    nomad
-    
-  
-    awscli
-    aws-sam-cli
-)
-CaskDevOpsToolList=(
-    vagrant
-    virtualbox
-    docker
-    vagrant-manager
-)
-if [ "$DevOps" != "${DevOps#[Yy]}" ] ;then
-    echo Yes
-    brew install ${DevOpsToolList[@]}
-    brew install --cask ${CaskDevOpsToolList[@]}
-
-    ## DOCKER APP
-    wget -P ~/Downloads/ https://github.com/docker/app/releases/download/v0.6.0/docker-app-darwin.tar.gz
-    tar -xvf ~/Downloads/docker-app-darwin.tar.gz -C ~/Downloads/
-    mv ~/Downloads/docker-app-darwin /usr/local/bin/docker-app
-    rm ~/Downloads/docker-app-darwin.tar.gz
-
-
-    ## Install AWS CLI
-    #pip3 --version
-    #curl -O https://bootstrap.pypa.io/get-pip.py
-    #python3 get-pip.py --user
-    #pip3 install awscli --upgrade --user
-    aws --version
-    #rm get-pip.py
-
-
-    curl "https://d1vvhvl2y92vvt.cloudfront.net/awscli-exe-macos.zip" -o "awscliv2.zip"
-    unzip awscliv2.zip
-    sudo ./aws/install
-    aws2 --version
-else
-    echo No
-fi
-
 
 ############# Productivity Tools #############
 beginDeploy "############# Productivity Tools #############"
@@ -242,10 +125,10 @@ CaskProductivityToolList=(
     slack
     gpg-suite
     zoomus
-    lastpass
     signal
     discord
-    github
+    virtualbox
+    spotify
 )
 if [ "$Productivity" != "${Productivity#[Yy]}" ] ;then
     echo Yes
@@ -256,63 +139,10 @@ fi
 
 
 ############# Mac Application #############
-beginDeploy "############# Mac Application #############"
-echo -n "Do you wish to install Mac Application (${bold}${green}y${reset}/${bold}${red}n${reset})? "
-read MacApplication
-
-MacApplicationToolList=(
-    409183694 # Keynote
-    409203825 # Numbers
-    409201541 # Pages
-    497799835 # Xcode
-    1450874784 # Transporter
-    1274495053 # Microsoft To Do
-    1295203466 # Microsoft Remote Desktop 10
-    985367838 # Microsoft Outlook
-)
-if [ "$MacApplication" != "${MacApplication#[Yy]}" ] ;then
-    brew install mas
-    mas install ${MacApplicationToolList[@]}
-
-    echo "######### Save screenshots to ${HOME}/Pictures/Screenshots"
-    defaults write com.apple.screencapture location -string "${HOME}/Pictures/Screenshots"
-
-    echo "######### Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF"
-    defaults write com.apple.screencapture type -string "png"
-
-else
-    echo No
-fi
 
 beginDeploy "############# CLEANING HOMEBREW #############"
 brew cleanup
 
-beginDeploy "############# GLOBAL GIT CONFIG #############"
-sh -c 'curl -s https://raw.githubusercontent.com/maxyermayank/developer-mac-setup/master/.gitignore >> ~/.gitignore'
-git config --global push.default current
-git config --global core.excludesfile ~/.gitignore
-git config --global user.name "<username>"
-git config --global user.email <email>
-git config --global color.branch auto
-git config --global color.diff auto
-git config --global color.interactive auto
-git config --global color.status auto
-
-beginDeploy "############# ALIASES #############"
-beginDeploy "############# GIT ALIASES #############"
-sh -c 'curl -s https://raw.githubusercontent.com/maxyermayank/developer-mac-setup/master/.git_aliases >> ~/.git_aliases'
-source ~/.git_aliases
-
-beginDeploy "############# DOCKER ALIASES #############"
-sh -c 'curl -s https://raw.githubusercontent.com/maxyermayank/developer-mac-setup/master/.docker_aliases >> ~/.docker_aliases'
-source ~/.docker_aliases
-
-beginDeploy "############# K8s ALIASES #############"
-sh -c 'curl -s https://raw.githubusercontent.com/maxyermayank/developer-mac-setup/master/.kubectl_aliases >> ~/.kubectl_aliases'
-source ~/.kubectl_aliases
-
-beginDeploy "############# SETUP BASH PROFILE #############"
-source ~/.bash_profile
 
 runtime=$((($(date +%s)-$start)/60))
 beginDeploy "############# Total Setup Time ############# $runtime Minutes"
